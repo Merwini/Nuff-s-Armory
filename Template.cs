16d82a2e1cb77f2082cbd2d77dcf0063fb37d52f
Merwini <86023141+Merwini@users.noreply.github.com>
2023-11-09 13:29:43 -0500

    Add project files.

---
+++ Template.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Nuff_s_Armory
+{
+    [StaticConstructorOnStartup]
+    public partial class Base
+    {
+
+    }
+}
